e020b54f10e1de522bf478e2f3476fccab380d43
FAIZ <FAIZ@192.168.0.4>
2019-04-27 01:30:00 +0530

    third n

--- Apps/src/main/java/org/apps/configuration/SwaggerConfiguration.java
+++ Apps/src/main/java/org/apps/configuration/SwaggerConfiguration.java
@@ -5,9 +5,44 @@ package org.apps.configuration;
  *
  */
 
-import org.springframework.context.annotation.Configuration;
+import java.util.Collections;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
 @Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = { "org.apps.controller" })
 public class SwaggerConfiguration {
+
+	@Bean
+	@Autowired
+	@Order(value = 3)
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.withClassAnnotation(RestController.class)).paths(PathSelectors.any())
+				.build().apiInfo(apiInfo());
+
+	}
+
+	private ApiInfo apiInfo() {
+		return new ApiInfo("Apps API", "Apps API Information", "APPS is centralized ERP service provided by fzapps",
+				"Terms of service", new Contact("FZAPPS", "www.fzapps.com", "admin@fzapps.com"), "License of API",
+				"API license URL", Collections.emptyList());
+	}
 
 }
--- Apps/src/main/java/org/apps/controller/DataController.java
+++ Apps/src/main/java/org/apps/controller/DataController.java
@@ -1,17 +1,32 @@
 package org.apps.controller;
 
+import java.util.ResourceBundle;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+@Api(tags = "Master Data API")
 @RestController
 public class DataController {
+	
+	ResourceBundle i18n = ResourceBundle.getBundle("ActionMessages");
+	
+	@Autowired
+	MessageSource messageSource;
+	
+	@ApiOperation(value = "Master Data api", notes = "Test api")
 	@RequestMapping(value="/",method=RequestMethod.GET)
 	public String getWelcomePage() {
 		return "welcome";
 	}
 	
+	@ApiOperation(value = "Master Data api", notes = "Test api")
 	@RequestMapping(value="/hello",method=RequestMethod.GET)
 	public String getHelloPage() {
 		return "hello";
